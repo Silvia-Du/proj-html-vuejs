0b073f02dc0bbde30bd5f496884080f611b22ef5
Silvia-Du <silviadu@hotmail.it>
2022-05-27 12:54:52 +0200

    Fine AboutComp provvisorio

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    
+  <div class="about">
+    <div class="container debug">
+      <div class="row debug py-5">
+
+        <div class="col-12 col-md-5 debug left py-5 d-flex justify-content-center flex-column align-items-center">
+          <h2 class="mb-5">Hey there, my name is John Doe. I'm the founder of the Brain Academi.</h2>
+          <img src="../assets/img/sign.jpg" alt="signature.jpg">
+        </div>
+        <div class="col-12 col-md-7 debug d-flex justify-content-center align-items-center">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo doloribus facere perferendis? Fugit itaque id cupiditate maiores esse distinctio? Placeat eum impedit suscipit recusandae nam velit nemo exercitationem minus hic cupiditate, vero dolore facilis, cum facere eius expedita adipisci error, vitae quidem! Eligendi harum reiciendis ex repellendus nemo debitis inventore?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus ducimus omnis molestias veniam labore temporibus quam quos! Blanditiis sint nobis officiis autem eaque ex sunt omnis nemo facere explicabo.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +22,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+  .about{
+    background-color: white;
+    
+  }
 
 </style>
