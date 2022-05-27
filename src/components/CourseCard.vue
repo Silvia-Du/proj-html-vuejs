8bd5af85337ed7b4c1e3579645adf8416b55590f
Silvia-Du <silviadu@hotmail.it>
2022-05-27 13:49:17 +0200

    Creato componente card

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,72 @@
+<template>
+  
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        
+    <div class="col">
+
+      <img src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="music-img">
+
+      <div class="text-box debug">
+        <p class="title pt-4">Title</p>
+        <p class="subtitle">Subtitle</p>
+
+        <div class="data d-flex justify-content-around debug py-2">
+
+          <div class="left box">
+            <i class="fa-solid fa-signal"></i>
+            <span>Advanced</span>
+          </div>
+          <div class="center box">
+            <i class="fa-solid fa-list-ul"></i>
+            <span>N' of Lecture</span>
+          </div>
+          <div class="right box">
+            <i class="fa-regular fa-clock"></i>
+            <span>H' of course</span>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'CourseCard'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/vars.scss';
+
+  img{
+    width: 100%;
+  }
+
+ .text-box{
+      .title{
+        font-size: 1.2rem;
+        font-weight: bold;
+        color: $titleColor;
+      }
+      .subtitle{
+        color: $textGray;
+      }
+
+      .data{
+        border-top: 1px solid $textGray;
+        font-size: 0.8rem;
+        color: $textDarkGray;
+        i{
+          color: $mainYellow;
+          font-size: 0.7rem;
+          margin-right: 5px;
+        }
+      }
+    }
+
+</style>
--- src/components/CoursesComp.vue
+++ src/components/CoursesComp.vue
@@ -2,32 +2,7 @@
   <div class="courses pt-5 pb-2 text-center">
     <h2 class="mb-4">New Courses</h2>
     <div class="container debug">
-      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-        
-        <div class="col">
-          <img src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="music-img">
-          <div class="text-box debug">
-
-            <p class="title pt-4">Title</p>
-            <p class="subtitle">Subtitle</p>
-
-              <div class="data d-flex justify-content-around debug py-2">
-              <div class="left box">
-                <i class="fa-solid fa-signal"></i>
-                <span>Advanced</span>
-              </div>
-              <div class="center box">
-                <i class="fa-solid fa-list-ul"></i>
-                <span>N' of Lecture</span>
-              </div>
-              <div class="right box">
-                <i class="fa-regular fa-clock"></i>
-                <span>H' of course</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+     <CourseCard />
 
     </div>
     <button type="button" class="btn btn-primary rounded-pill my-5">Load More</button>
@@ -35,8 +10,10 @@
 </template>
 
 <script>
+import CourseCard from './CourseCard.vue';
 export default {
-  name: 'CoursesComp'
+    name: "CoursesComp",
+    components: { CourseCard }
 }
 </script>
 
@@ -47,9 +24,7 @@ export default {
 
   .courses{
     background-color: #edf2f5;
-    img{
-      width: 100%;
-    }
+   
     .btn{
       background-color: #f2b91e;
       border-color: #f2b91e;
@@ -57,27 +32,7 @@ export default {
       font-weight: bold;
       padding: 10px 20px;
     }
-    .text-box{
-      .title{
-        font-size: 1.2rem;
-        font-weight: bold;
-        color: $titleColor;
-      }
-      .subtitle{
-        color: $textGray;
-      }
-
-      .data{
-        border-top: 1px solid $textGray;
-        font-size: 0.8rem;
-        color: $textDarkGray;
-        i{
-          color: $mainYellow;
-          font-size: 0.7rem;
-          margin-right: 5px;
-        }
-      }
-    }
+   
   }
 
 </style>
